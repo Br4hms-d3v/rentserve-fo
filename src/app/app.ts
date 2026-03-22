4b401f7bc238dd4746aba624a0b85bdd3c114f72
Brahms <bramso.brahms@gmail.com>
2026-03-22 01:09:42 +0100

    Create project RentServe

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.less'
+})
+export class App {
+  protected readonly title = signal('TFE_RentServe_Frontend');
+}
