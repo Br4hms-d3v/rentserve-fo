18e18a5d318606936aac864116a04a67bccca4a3
Brahms <bramso.brahms@gmail.com>
2026-08-12 22:12:10 +0200

    Header to change theme and language

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/layouts/header/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, Header],
   templateUrl: './app.html',
   styleUrl: './app.less',
 })
