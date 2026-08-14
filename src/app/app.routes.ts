2dfdee5a77159f72aa8b32e66dbac7e1ea1c62e1
Brahms <bramso.brahms@gmail.com>
2026-08-14 19:24:23 +0200

    Add two route to redirect home page and authentification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren:() => import('./features/home/home.routes').then((r) => r.home_routes)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((r) => r.auth_routes)
+  },
+];
