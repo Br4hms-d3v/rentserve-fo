45135ddeefd839d378b601fe754c7b5117b81e37
Brahms <bramso.brahms@gmail.com>
2026-08-27 19:40:30 +0200

    Generate dashboard with redirection from login

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { TuiButton, TuiIcon, TuiInput } from '@taiga-ui/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { TuiButton, TuiIcon, TuiInput, TuiNotification } from '@taiga-ui/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../service/auth-service';
 import { ThemeService } from '../../../core/services/ThemeService';
 import { LoginModel } from '../model/login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, TuiIcon, TuiInput, TuiButton],
+  imports: [ReactiveFormsModule, TuiIcon, TuiInput, TuiButton, TuiNotification],
   templateUrl: './login.html',
   styleUrl: './login.less',
 })
 export class Login implements OnInit {
   isDarkMode = false; // Change theme from light to dark
+  protected readonly show = signal(false); // Show notification
+  protected readonly isSuccess = signal(false); // Change color Green for succes or Red error
   protected messageError = '';
-  protected messageSuccess = '';
+  titleError = '';
 
   constructor(
     private readonly _authService: AuthService, // Service to call login from Backend
     private themeService: ThemeService, // Call the service to change color theme
+    private readonly _router: Router, // Tool to navigate
   ) {}
 
   ngOnInit() {
@@ -42,14 +46,15 @@ export class Login implements OnInit {
     }
 
     this._authService.login(<LoginModel>this.loginForm.value).subscribe({
-      next:(user) => {
-        this.messageSuccess = "BonCode";
-        console.log(this.messageSuccess)
+      next: (user) => {
+        this._router.navigate(['/dashboard']).then();
       },
       error: (error) => {
-        this.messageError = "mot de passe faux";
-        console.log(this.messageError);
-      }
-    })
+        this.messageError = "Le mot de passe ou le nom d'utilisateur est érroné"
+        this.titleError = "Un problème est survenu";
+        this.isSuccess.set(false);
+        this.show.set(true);
+      },
+    });
   }
 }
--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home/home';
+import { Dashboard } from './dashboard/dashboard';
 
 export const home_routes: Routes = [
   { path: '', redirectTo: 'rent-serve', pathMatch: 'full' },
   { path: 'rent-serve', component: Home },
+  { path: 'dashboard', component: Dashboard}
 ];
