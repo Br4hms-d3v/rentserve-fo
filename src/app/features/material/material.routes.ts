b5566a2776421c6bde968950d985139c654f9802
Brahms <bramso.brahms@gmail.com>
2026-08-31 13:05:28 +0200

    Add route for each dropdown menu at material

--- src/app/features/material/material.routes.ts
+++ src/app/features/material/material.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { MaterialList } from './material-list/material-list';
 import { authGuard } from '../../core/services/authGuard';
+import { MaterialEdit } from './material-edit/material-edit';
+import { MaterialCreate } from './material-create/material-create';
+import { MaterialById } from './material-by-id/material-by-id';
 
 export const material_routes: Routes = [
-  {path: 'all-materials', component: MaterialList, canActivate: [authGuard]}
+  {path: 'all-materials', component: MaterialList, canActivate: [authGuard]},
+  {path: ':id/edit', component: MaterialEdit, canActivate: [authGuard]},
+  {path: 'create/new-material', component: MaterialCreate, canActivate: [authGuard]},
 ]
