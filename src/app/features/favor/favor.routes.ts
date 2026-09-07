963d755d92d462883c64b45f37d4497c2e6690d0
Brahms <bramso.brahms@gmail.com>
2026-09-08 01:46:15 +0200

    Route for edit favor by id

--- src/app/features/favor/favor.routes.ts
+++ src/app/features/favor/favor.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { authGuard } from '../../core/services/authGuard';
 import { FavorList } from './favor-list/favor-list';
+import { FavorEdit } from './favor-edit/favor-edit';
 
 export const favor_routes: Routes = [
   { path: 'all-favour', component: FavorList, canActivate: [authGuard] },
+  { path: ':id/edit', component: FavorEdit, canActivate: [authGuard] },
 ];
