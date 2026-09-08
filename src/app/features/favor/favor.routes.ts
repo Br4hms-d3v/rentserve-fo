45d6bc836da4de8d574211539da4083dfdb1eafd
Brahms <bramso.brahms@gmail.com>
2026-09-09 00:43:23 +0200

    Add a route to create a new favor

--- src/app/features/favor/favor.routes.ts
+++ src/app/features/favor/favor.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { authGuard } from '../../core/services/authGuard';
 import { FavorList } from './favor-list/favor-list';
 import { FavorEdit } from './favor-edit/favor-edit';
+import { FavorCreate } from './favor-create/favor-create';
 
 export const favor_routes: Routes = [
   { path: 'all-favour', component: FavorList, canActivate: [authGuard] },
   { path: ':id/edit', component: FavorEdit, canActivate: [authGuard] },
+  { path: 'create/new-favor', component: FavorCreate, canActivate: [authGuard] },
 ];
