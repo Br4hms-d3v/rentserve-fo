406737764dd67074f0ff1bfcbeeae2bccc304fd5
Brahms <bramso.brahms@gmail.com>
2026-09-10 04:06:30 +0200

    add submit to save edit user

--- src/app/features/user/service/user-service.ts
+++ src/app/features/user/service/user-service.ts
@@ -2,6 +2,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environment/environment';
 import { UserModel } from '../model/user';
+import { UserForm } from '../model/userForm';
+import { UserTokenModel } from '../model/userToken';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -28,5 +31,30 @@ export class UserService {
   getUser(id: number) {
     const headers = this.getAuthHeader();
     return this._http.get<UserModel>(this.apiUrl + id, { headers });
+  }
+
+  editUser(id: number, form: UserForm) {
+    const headers = this.getAuthHeader();
+    return this._http.put<UserTokenModel>(this.apiUrl + id + '/edit', form, { headers }).pipe(
+      tap((response) => {
+        this.updateLocalStoredToken(response);
+      }),
+    );
+  }
+
+  private updateLocalStoredToken(response: UserTokenModel) {
+    const userJson = localStorage.getItem('currentUser');
+    if (!userJson) return;
+
+    const currentUser = JSON.parse(userJson);
+
+    // On fusionne les nouvelles infos (pseudo, email, etc.) et surtout le nouveau token
+    const updatedUser = {
+      ...currentUser,
+      ...response,
+      token: response.token,
+    };
+
+    localStorage.setItem('currentUser', JSON.stringify(updatedUser));
   }
 }
