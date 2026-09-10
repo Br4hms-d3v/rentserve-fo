5ce0a701a9691db4a32db4fb366e623007a1db47
Brahms <bramso.brahms@gmail.com>
2026-09-11 00:42:35 +0200

    Add Color theme

--- src/app/features/user/user-profile/user-profile.ts
+++ src/app/features/user/user-profile/user-profile.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input, OnInit } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import {
   NavigationStart,
@@ -11,6 +11,7 @@ import { filter, map, pairwise } from 'rxjs';
 import { NgClass } from '@angular/common';
 import { TuiSlides } from '@taiga-ui/layout';
 import { TuiTabs } from '@taiga-ui/kit';
+import { ThemeService } from '../../../core/services/ThemeService';
 
 @Component({
   selector: 'app-user-profile',
@@ -19,9 +20,15 @@ import { TuiTabs } from '@taiga-ui/kit';
   styleUrl: './user-profile.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserProfile {
+export class UserProfile implements OnInit{
+  private _themeService = inject(ThemeService); // Call the service to change color theme
+
   @Input() userId!: string;
   isDarkMode = false; // Change theme from light to dark
+
+  ngOnInit(): void {
+    this.changeTheme();
+  }
 
   protected readonly direction = toSignal(
     inject(Router).events.pipe(
@@ -42,4 +49,9 @@ export class UserProfile {
     ),
     { initialValue: 1 },
   );
+
+  changeTheme() {
+    this.isDarkMode = this._themeService.isDarkMode(); // Get current theme
+    this._themeService.darkMode$.subscribe((mode: boolean) => (this.isDarkMode = mode)); // Watch changes in dark mode (reactive)
+  }
 }
