488671fa62a7869bd5590071410faebdea0dd4dd
Brahms <bramso.brahms@gmail.com>
2026-09-12 22:13:35 +0200

    Change dropMenu to Drawer

--- src/app/shared/layouts/nav/nav/nav.ts
+++ src/app/shared/layouts/nav/nav/nav.ts
@@ -1,34 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { TuiButton, TuiDataList, TuiDialogService, TuiDropdown, TuiOption } from '@taiga-ui/core';
+import { TuiButton, TuiClose, TuiDialogService, TuiPopup, TuiTitle } from '@taiga-ui/core';
 import { ThemeService } from '../../../../core/services/ThemeService';
 
-import { TuiChevron } from '@taiga-ui/kit';
 import { AuthService } from '../../../../features/auth/service/auth-service';
+import { FormControl } from '@angular/forms';
+import { TuiDrawer } from '@taiga-ui/kit';
+import { TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-nav',
-  imports: [
-    CommonModule,
-    RouterLink,
-    TuiButton,
-    TuiDropdown,
-    TuiDataList,
-    TuiOption,
-    TuiDataList,
-    TuiChevron,
-  ],
+  imports: [NgClass, RouterLink, TuiButton, TuiDrawer, TuiPopup, TuiClose, TuiHeader, TuiTitle],
   templateUrl: './nav.html',
   styleUrl: './nav.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Nav implements OnInit {
   private readonly _authService = inject(AuthService);
   private readonly themeService = inject(ThemeService);
   private readonly router = inject(Router);
 
-  protected isOpen = false;
-  protected isAuthenticated = false;
+  protected readonly control = new FormControl('Some value');
+  protected readonly openDrawer = signal(false);
+
+  protected readonly isAuthenticated = signal(false);
   isDarkMode = false;
   protected firstname: string | null | undefined;
   protected userId: number | undefined;
@@ -39,12 +35,12 @@ export class Nav implements OnInit {
 
     this._authService.currentUser$.subscribe((user) => {
       if (user) {
-        this.isAuthenticated = true;
+        this.isAuthenticated.set(true);
         // console.log(this.isAuthenticated);
         this.firstname = user.firstName;
         this.userId = user.id;
       } else {
-        this.isAuthenticated = false;
+        this.isAuthenticated.set(false);
         this.firstname = null;
         this.userId = undefined;
       }
@@ -53,7 +49,16 @@ export class Nav implements OnInit {
 
   logout() {
     this._authService.logout();
+    this.openDrawer.set(false);
     this.router.navigate(['/']);
-    this.isAuthenticated = false;
+    this.isAuthenticated.set(false);
+  }
+
+  public onClose(): void {
+    if (this.control.pristine) {
+      this.openDrawer.set(false);
+
+      return;
+    }
   }
 }
