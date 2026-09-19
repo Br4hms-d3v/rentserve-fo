c1b1ee02af8ff98a86d13d7782c7f092277d13c8
Brahms <bramso.brahms@gmail.com>
2026-09-19 19:11:29 +0200

    Edit category for favor with theme color

--- src/app/features/category/category.routes.ts
+++ src/app/features/category/category.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CategoryMaterialEdit } from './category-material-edit/category-material-edit';
 import { authGuard } from '../../core/services/authGuard';
+import { CategoryFavorEdit } from './category-favor-edit/category-favor-edit';
 
 export const category_routes: Routes = [
   { path: ':id/edit-category', component: CategoryMaterialEdit, canActivate: [authGuard] },
+  { path: ':id/edit-favor', component: CategoryFavorEdit, canActivate: [authGuard] },
 ];
