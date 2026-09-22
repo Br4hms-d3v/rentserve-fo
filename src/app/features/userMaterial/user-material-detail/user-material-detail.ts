2dcf953a3131a9fc3ee8f058eeb5656d66adf44c
Brahms <bramso.brahms@gmail.com>
2026-09-23 01:52:12 +0200

    Display detail user marterial for only the owner

--- src/app/features/userMaterial/user-material-detail/user-material-detail.ts
+++ src/app/features/userMaterial/user-material-detail/user-material-detail.ts
@@ -1,6 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { UserMaterialService } from '../service/user-material-service';
 import { UserMaterialDetailModel } from '../model/user-material-detail';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-material-detail',
@@ -10,21 +11,23 @@ import { UserMaterialDetailModel } from '../model/user-material-detail';
 })
 export class UserMaterialDetail implements OnInit {
   private readonly _userMaterialService = inject(UserMaterialService);
+  private readonly _route = inject(ActivatedRoute); // Get the id of the material (by url)s
+  private readonly _cdr = inject(ChangeDetectorRef);
 
-  userMaterialDetail!: UserMaterialDetailModel;
+  userMaterialDetail: UserMaterialDetailModel | null = null;
   userMaterialId!: number;
 
   ngOnInit() {
-    this.userMaterialId = history.state.userMaterialId;
-    // console.log(this.userMaterialId);
-    this.getUserMaterialDetail();
+    this.userMaterialId = Number(this._route.snapshot.paramMap.get('id'));
+    this.userMaterialOwner();
   }
 
-  protected getUserMaterialDetail() {
+  protected userMaterialOwner() {
     this._userMaterialService.getUserMaterialDetail(this.userMaterialId).subscribe({
       next: (result) => {
         this.userMaterialDetail = result;
-        console.log(this.userMaterialDetail);
+
+        this._cdr.detectChanges();
       },
     });
   }
