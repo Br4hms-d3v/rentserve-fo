a28be5e2b6d9733563561d3c69b3c61d2115ff2f
Brahms <bramso.brahms@gmail.com>
2026-09-22 03:21:49 +0200

    generate user material by ID

---
+++ src/app/features/userMaterial/user-material-detail/user-material-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-material-detail',
+  imports: [],
+  templateUrl: './user-material-detail.html',
+  styleUrl: './user-material-detail.less',
+})
+export class UserMaterialDetail {}
