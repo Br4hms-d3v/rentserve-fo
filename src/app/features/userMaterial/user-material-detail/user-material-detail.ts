235401005899e96b814b7e2e02a71a27b18ab299
Brahms <bramso.brahms@gmail.com>
2026-09-23 20:26:03 +0200

    Add color theme for table detail user material

--- src/app/features/userMaterial/user-material-detail/user-material-detail.ts
+++ src/app/features/userMaterial/user-material-detail/user-material-detail.ts
@@ -2,10 +2,12 @@ import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { UserMaterialService } from '../service/user-material-service';
 import { UserMaterialDetailModel } from '../model/user-material-detail';
 import { ActivatedRoute } from '@angular/router';
+import { ThemeService } from '../../../core/services/ThemeService';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-user-material-detail',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './user-material-detail.html',
   styleUrl: './user-material-detail.less',
 })
@@ -13,13 +15,16 @@ export class UserMaterialDetail implements OnInit {
   private readonly _userMaterialService = inject(UserMaterialService);
   private readonly _route = inject(ActivatedRoute); // Get the id of the material (by url)s
   private readonly _cdr = inject(ChangeDetectorRef);
+  private _themeService = inject(ThemeService); // Call the service to change color theme
 
+  isDarkMode = false; // Change theme from light to dark
   userMaterialDetail: UserMaterialDetailModel | null = null;
   userMaterialId!: number;
 
   ngOnInit() {
     this.userMaterialId = Number(this._route.snapshot.paramMap.get('id'));
     this.userMaterialOwner();
+    this.changeTheme();
   }
 
   protected userMaterialOwner() {
@@ -30,5 +35,10 @@ export class UserMaterialDetail implements OnInit {
         this._cdr.detectChanges();
       },
     });
+  }
+
+  changeTheme() {
+    this.isDarkMode = this._themeService.isDarkMode(); // Get current theme
+    this._themeService.darkMode$.subscribe((mode: boolean) => (this.isDarkMode = mode)); // Watch changes in dark mode (reactive)
   }
 }
