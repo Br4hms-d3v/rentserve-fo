892587bd0b77d74fcdf26494ff1e5a8cbbc53fad
Brahms <bramso.brahms@gmail.com>
2026-09-25 01:32:25 +0200

    change price from bigint to number

--- src/app/features/userMaterial/model/user-material-detail.ts
+++ src/app/features/userMaterial/model/user-material-detail.ts
@@ -1,13 +1,14 @@
 import { MaterialModel } from '../../material/model/Material';
 import { UserPseudoModel } from '../../user/model/user-pseudo';
+import { State } from '../enum/state';
 
 export interface UserMaterialDetailModel {
   id: number;
   descriptionMaterial: string;
-  priceHourMaterial: bigint;
+  priceHourMaterial: number;
   isAvailable: boolean;
   pictures: string;
   user: UserPseudoModel;
   material: MaterialModel;
-  state: string;
+  state: State;
 }
